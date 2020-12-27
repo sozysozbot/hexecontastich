a0eadbee78fe590034ad4b211b8ebbad37654841
jekto_vatimeliju <jekto.vatimeliju@gmail.com>
2020-12-27 19:20:37 +0900

    Finally read from file and generate

--- src/main.rs
+++ src/main.rs
@@ -11,77 +11,25 @@ use std::fs::File;
 use std::io::Write;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    write_files(
-        "2020-09-26",
-        &vec![
-            "
-'I:gakinA:sebi'AntegonI:si:
-se:sUperi:'a:gAntasorI:'a
-'a:kasinE:raga'A:ri:ni:sEko
-ge:te:rImba'asAkabe:nI:
-dEhtonagi:rAse'a:ka:sUhte
-sa:magarIntisisAkare:nI:
-sIssatinA:gabigEntegonI:si:
-se:ramirA:gambOhteganI:
-si:si:'Anteraka:sarinIsse
-'a:kagipIssekAre:rimanI:
-dAnkatina:sAhtantibinEsse
-'a:ka:kAssiritAhteganI:
-bi:rine'a:rAkiga:na:'Ihti
-'IhtoresAntanisUhtabi'Onda",
-            "
-mi:si:'a:tInatankeronAhte
-kantetinIssi'akAsene:mI:
-nihtina'a:gAse'a:rina'Ihte
-kAssaritIndasakAsere:mI:
-bi:sakinAnte'a'a:gasirIhte
-'intasenAhti'Ana:gisemI:
-ni:rise'Andiki'A:samarIhti
-'i:ri:bAsena:'AssinanI:
-dihtire'Ahpisira:ka:se:nAsi
-ni:bise'Ahtana'Irome:nI:",
-            "
-ne:sIgi'assAre'a:ka:nAnte
-ti:rekirI:rekanAsene:mI:
-kuhtina'IndesirA:nisomInde
-nIsi'amAse:ratinIsige:mI:
-mi:miserAntani'Asse'orI:re
-dihtesinImbanibAsene:mI:
-nindisomAhterana:gaserIhti
-ni:ni:sAndire'AssabagOhta",
-            "
-'a:sUpana:'a:nIhtinasEndi
-se:se:pOturi:'AssagarI:ra
-ne:be:'a:tInasi:rakurAhti
-'inkOpeni:ni:sAntegorI:na
-'o:nOhte:'a:gAntari'Asse
-nOhtari'I:nakorIsare:sI:
-ti:gInane:ri:'AhtinarIhti
-mEhtako'i:rIse'Agone:sI:
-'o:nOhte:mi:'AnteginIssa
-de:re:sAturi:nAsome:nI:
-ni:'Ameka:ta:gAssatinAhta
-si:si:'AhteribIndabinAhta",
-            "
-sa:'a:re:gAsi'a:gasenInti
-me:ne:tOgusiri'Asene:mI:
-ti:nIsema:na:bEntasinAhte
-na:rInase:ni:nAsene:mI:
-si:sitinAhtiri'AndasinAhte
-ni:ni:sa:tIrinIsige:mI:
-kOhtorisImbasebEhtina'Indi
-be:mi:rIhtasinAhtegomI:
-'angasinIntemigIhteribIhti
-so:sUri'a:ga:sAtine:mI:
-se:be:'AssirinIndasinAnde
-'Isima:'a:bIsesApire:mI:
-mEssirinIntami'AnderemIssi
-ma:nApari:ni:rUhteribImba",
-            "
-nampasinIhtera'a:nisirIngi
-'a:ka:nIsige:'AhtabamI:",
-        ],
-    )
+    for entry in std::fs::read_dir("raw/")? {
+        let entry = entry?;
+        let path = entry.path();
+        if !path.is_dir() {
+            let date = entry.file_name().into_string().unwrap();
+
+            println!("{:?}", date);
+            let content = std::fs::read_to_string(format!("raw/{}", date))?;
+
+            // to convert \r\n into \n
+            let content = content.lines().collect::<Vec<_>>().join("\n");
+
+            let cont = content.split("\n\n").collect::<Vec<_>>();
+
+            write_files(&date, &cont)?;
+        }
+    }
+
+    Ok(())
 }
 
 fn write_files(date: &str, data: &[&str]) -> Result<(), Box<dyn Error>> {
